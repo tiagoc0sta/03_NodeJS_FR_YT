5fcefe2d6507904970d2f679b32a2b1f69469aea
Tiago Costa <tiagodpv@gmail.com>
2023-04-28 19:21:21 -0400

    add connection

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,18 +1,30 @@
 const mongoose = require('mongoose');
 
+// const connectToDatabase = async () => {
+//   await mongoose.connect(
+//     `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejsdicasparadev.lncg9ob.mongodb.net/database?retryWrites=true&w=majority`,
+//     (error) => {
+//       if (error) {
+//         return console.log(
+//           'Ocorreu um erro ao se conectar ao Banco de dados: ',
+//           error
+//         );
+//       }
+//       return console.log('Conexão ao banco de dados realizada com sucesso');
+//     }
+//   );
+// };
+
 const connectToDatabase = async () => {
-  await mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejsdicasparadev.lncg9ob.mongodb.net/database?retryWrites=true&w=majority`,
-    (error) => {
-      if (error) {
-        return console.log(
-          'Ocorreu um erro ao se conectar ao Banco de dados: ',
-          error
-        );
-      }
-      return console.log('Conexão ao banco de dados realizada com sucesso');
-    }
-  );
+  try {
+    mongoose.set('strictQuery', false);
+    mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejsdicasparadev.lncg9ob.mongodb.net/database?retryWrites=true&w=majority`
+    );
+    console.log('Connected to Mongo Successfully!');
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = connectToDatabase;
