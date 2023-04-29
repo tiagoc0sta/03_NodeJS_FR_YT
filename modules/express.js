d9eabff04c3c37288d5eb6771b767d7b028628df
Tiago Costa <tiagodpv@gmail.com>
2023-04-29 16:40:31 -0400

    add CRUDE

--- modules/express.js
+++ modules/express.js
@@ -2,30 +2,66 @@ const express = require('express');
 const UserModel = require('../src/models/user.model');
 
 const app = express();
+app.use(express.json());
 
-app.get('/home', (req, res) => {
-  res.status(200).send('<h1>Hello World</h1>');
+//buscar users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await UserModel.find();
+
+    res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 });
 
-app.get('/users', (req, res) => {
-  const users = [
-    {
-      name: 'Jhon Doe',
-      email: 'JD@gmail.com',
-    },
-    {
-      name: 'Jane Doe',
-      email: 'Jane@gmail.com',
-    },
-  ];
+//Criar user
+app.post('/users', async (req, res) => {
+  try {
+    const user = UserModel.create(req.body);
 
-  res.status(200).json(users);
+    res.status(201).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 });
 
-app.post('/users', (req, res) => {
-  const user = UserModel.create(req.body);
+//buscar user por ID
+app.get('/users/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
 
-  res.status(201).json(user);
+    const user = await UserModel.findById(id);
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+//alterar apenas um ou mais dados do usuario - sem alterar o usuario totalmente
+app.patch('/users/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+//deletar usuario
+app.delete('/users/:id', async function (req, res) {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndRemove(id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 const port = 8080;
